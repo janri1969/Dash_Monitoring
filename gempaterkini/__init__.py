04615d7d4df1440fad4e3e42c05e2d198ce2373b
janri1969 <checkerok@yahoo.com>
2022-08-11 17:23:14 +0700

    gempak terkini

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,24 @@
+"""
+Aplikasi gempa terkini dari BMKG
+"""
+
+def ekstraksi_data():
+    hasil=dict()
+    hasil['tanggal'] = '24 Agustuss 2021'
+    hasil['waktu'] = '12:05:52 WIB'
+    hasil['magnitudo'] = 4.0
+    hasil['lokasi'] = {'ls':1.48, 'bt':134.01}
+    hasil['pusat'] = 'Pusat gempa berada di darat 18 KM barat laut ransiki'
+    hasil['dirasakan'] = 'Dirasakan (skala MMI): II-III Manokwari, II-III Ransiki'
+
+    return hasil
+
+def tampilkan_data(result):
+    print("Gempa Terakhir berdasarkan BMKG")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Lokasi : LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
+    print(f"{result['pusat']}")
+    print(f"{result['dirasakan']}")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Aplikasi monitoring
+"""
+from gempaterkini import ekstraksi_data, tampilkan_data
+
+if __name__ == '__main__':
+    print('aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
