31dfb4a9ee410fe7ff3e9aad69bdb322b4d8a2b2
janri1969 <checkerok@yahoo.com>
2022-08-18 17:42:29 +0700

    finish coding gempa terkini package BeautifulSoup dan Requests

--- Beritaterpopuler/__init__.py
+++ Beritaterpopuler/__init__.py
@@ -1,5 +1,5 @@
 """
-Aplikasi gempa terkini dari BMKG 1
+Aplikasi Berita Terpopuler pada detik.com
 """
 import requests
 from bs4 import BeautifulSoup
@@ -7,22 +7,22 @@ from bs4 import BeautifulSoup
 def ekstraksi_data():
 
     try:
-        content = requests.get("https://bmkg.go.id")
-
+        content = requests.get("https://detik.com")
+#        print(content.text)
     except Exception:
         return None
 
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('span', {'class' : 'waktu'})
-        result = result.text.split(', ')
 
+        result = soup.find('span', {'class' : 'waktu'})
+        print(result)
+        result = result.text.split(', ')
+        print(result)
         tanggal = result[0]
         waktu = result[1]
 
         result = soup.find('div', {'class' : 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
-
         i = 0
         magnitudo = None
         kedalaman = None
@@ -31,21 +31,19 @@ def ekstraksi_data():
         lokasi = None
         dirasakan = None
 
-        for res in result:
+        for rest in result:
             if i == 1:
-                magnitudo = res.text
-
+                magnitudo = rest.text
             elif i == 2:
-                kedalaman = res.text
+                kedalaman = rest.text
             elif i == 3:
-#                koordinat = res.text.split[' - ']
-                 koordinat = res.text
-#                ls = koordinat[0]
-#                bt = koordinat[1]
+                koordinat = rest.text.split[' - ']
+                ls = koordinat[0]
+                bt = koordinat[1]
             elif i == 4:
-                lokasi = res.text
+                lokasi = rest.text
             elif i == 5:
-                dirasakan = res.text
+                dirasakan = rest.text
             i = i + 1
 
         hasil = dict()
@@ -53,8 +51,7 @@ def ekstraksi_data():
         hasil['waktu'] = waktu
         hasil['magnitudo'] = magnitudo
         hasil['kedalaman'] = kedalaman
-#        hasil['koordinat'] = {'ls' : ls, 'bt' : bt}
-        hasil['koordinat'] = koordinat
+        hasil['koordinat'] = {'ls' : ls, 'bt' : bt}
         hasil['lokasi'] = lokasi
         hasil['dirasakan'] = dirasakan
         return hasil
@@ -62,7 +59,6 @@ def ekstraksi_data():
         return None
 
 def tampilkan_data(result):
-
     if result is None:
         print("Tidak menemukan data gempa terkini")
         return
@@ -71,9 +67,7 @@ def tampilkan_data(result):
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
     print(f"Magnitudo {result['magnitudo']}")
-    print(f"Kedalaman {result['kedalaman']}")
-    print(f"Lokasi {result['lokasi']}")
+#    print(f"Kedalaman {result['kedalaman']}")
+#    print(f"Lokasi {result['lokasi']}")
 #    print(f"Koordinat : LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
-    print(f"Koordinat {result['koordinat']}")
-    print(f"Dirasakan {result['dirasakan']}")
-
+#    print(f"Dirasakan {result['dirasakan']}")
