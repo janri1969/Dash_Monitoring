fc23baa7abc3986bc8e997fb9dd6d5d6145056a2
janri1969 <checkerok@yahoo.com>
2022-08-22 16:20:27 +0700

    finish coding gempa terkini package BeautifulSoup dan Requests

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -8,7 +8,6 @@ def ekstraksi_data():
 
     try:
         content = requests.get("https://bmkg.go.id")
-
     except Exception:
         return None
 
@@ -19,10 +18,12 @@ def ekstraksi_data():
 
         tanggal = result[0]
         waktu = result[1]
-
+#        print(tanggal)
+#        print(waktu)
+#        print(result)
         result = soup.find('div', {'class' : 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
         result = result.findChildren('li')
-
+#        print(result)
         i = 0
         magnitudo = None
         kedalaman = None
@@ -38,10 +39,10 @@ def ekstraksi_data():
             elif i == 2:
                 kedalaman = res.text
             elif i == 3:
-#                koordinat = res.text.split[' - ']
-                 koordinat = res.text
-#                ls = koordinat[0]
-#                bt = koordinat[1]
+                koordinat = res.text.split('- ')
+#                 koordinat = res.text
+                ls = koordinat[0]
+                bt = koordinat[1]
             elif i == 4:
                 lokasi = res.text
             elif i == 5:
@@ -53,8 +54,7 @@ def ekstraksi_data():
         hasil['waktu'] = waktu
         hasil['magnitudo'] = magnitudo
         hasil['kedalaman'] = kedalaman
-#        hasil['koordinat'] = {'ls' : ls, 'bt' : bt}
-        hasil['koordinat'] = koordinat
+        hasil['koordinat'] = {'ls' : ls, 'bt' : bt}
         hasil['lokasi'] = lokasi
         hasil['dirasakan'] = dirasakan
         return hasil
@@ -76,5 +76,3 @@ def tampilkan_data(result):
 #    print(f"Koordinat : LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
     print(f"Koordinat {result['koordinat']}")
     print(f"Dirasakan {result['dirasakan']}")
-#
-#
