173f06b0557e56da0e9d23209af9ff81d3926130
janri1969 <checkerok@yahoo.com>
2022-08-31 17:24:03 +0700

    berita terpopuler

--- Beritaterpopuler/__init__.py
+++ Beritaterpopuler/__init__.py
@@ -1,78 +1,20 @@
 """
-Aplikasi Berita Terpopuler
+Aplikasi Berita Terpopuler detik.com
 """
 import requests
 from bs4 import BeautifulSoup
 
-def ekstraksi_data():
+html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
 
-    try:
-        content = requests.get("https://detik.com")
-    except Exception:
-        return None
+soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-    if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('span', {'class' : 'waktu'})
-        result = result.text.split(', ')
+populer_area = soup.find(attrs={'class':'grid-row list-content'})
 
-        tanggal = result[0]
-        waktu = result[1]
-#        print(tanggal)
-#        print(waktu)
-#        print(result)
-        result = soup.find('div', {'class' : 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
-#        print(result)
-        i = 0
-        magnitudo = None
-        kedalaman = None
-        ls = None
-        bt = None
-        lokasi = None
-        dirasakan = None
+titles = populer_area.findAll(attrs={'class':'media__title'})
+images = populer_area.findAll(attrs={'class':'media__image'})
 
-        for res in result:
-            if i == 1:
-                magnitudo = res.text
+for image in images:
+    print(image.find('a').find('img')['title'])
 
-            elif i == 2:
-                kedalaman = res.text
-            elif i == 3:
-                koordinat = res.text.split('- ')
-#                 koordinat = res.text
-                ls = koordinat[0]
-                bt = koordinat[1]
-            elif i == 4:
-                lokasi = res.text
-            elif i == 5:
-                dirasakan = res.text
-            i = i + 1
-
-        hasil = dict()
-        hasil['tanggal'] = tanggal
-        hasil['waktu'] = waktu
-        hasil['magnitudo'] = magnitudo
-        hasil['kedalaman'] = kedalaman
-        hasil['koordinat'] = {'ls' : ls, 'bt' : bt}
-        hasil['lokasi'] = lokasi
-        hasil['dirasakan'] = dirasakan
-        return hasil
-    else:
-        return None
-
-def tampilkan_data(result):
-
-    if result is None:
-        print("Tidak menemukan data gempa terkini")
-        return
-
-    print("Gempa Terakhir berdasarkan BMKG")
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Kedalaman {result['kedalaman']}")
-    print(f"Lokasi {result['lokasi']}")
-    print(f"Koordinat : LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
-    print(f"{result['dirasakan']}")
+#print(titles)
 
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 """
 Aplikasi monitoring terkini 1
 """
-from gempaterkini import ekstraksi_data, tampilkan_data
+#from gempaterkini import ekstraksi_data, tampilkan_data
+#from Beritaterpopuler import ekstraksi_data, tampilkan_data
 
-import gempaterkini
+#import gempaterkini
+
+#if __name__ == '__main__':
+#    print('aplikasi utama')
+#    result = ekstraksi_data()
+#    tampilkan_data(result)
+
+import requests
+from bs4 import BeautifulSoup
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/Beritaterpopuler')
+def Beritaterpopuler():
+    html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
+
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    populer_area = soup.find(attrs={'class': 'grid-row list-content'})
+
+    titles = populer_area.findAll(attrs={'class': 'media__title'})
+    images = populer_area.findAll(attrs={'class': 'media__image'})
+
+    return render_template('index.html', images=images)
 
 if __name__ == '__main__':
-    print('aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    app.run(debug=True)
